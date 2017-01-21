2a1e0832e716bca9595d9ba395fbf4b34ad53b4e
azapalac <azapalac@ucsc.edu>
2017-01-20 21:14:35 -0800

    Test commit

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public float movementLimit = 3;
+    public float maxMoveSpeed, minMoveSpeed;
     public float moveSpeed;
     public float currentMovementPosition = 0;
 
