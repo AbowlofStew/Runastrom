8a677f59c05e141deec91d1ce17c9d96d29f0f5f
acarerk <acarerk@gmail.com>
2017-01-21 11:01:03 -0800

    wave_spawner

--- Assets/WaveHandler.cs
+++ Assets/WaveHandler.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaveHandler : MonoBehaviour {
-
+    Vector2 spawnloc;
+    float x, y;
+    float timer;
+    public float spawntime = 1;
+    public GameObject Wave;
 	// Use this for initialization
 	void Start () {
-		
+       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        timer += Time.deltaTime;
+        if (timer >= spawntime)
+        {
+            timer = 0;
+            spawnWave();
+        }
 	}
+    public void generateSpawnLoc()
+    {
+        x = Random.Range(0,-200);
+        x = x / 100;
+        y = (-1*x) - 2;
+        spawnloc = new Vector2(x, y);
+    }
+    public void spawnWave()
+    {
+        generateSpawnLoc();
+        Instantiate(Wave, spawnloc, new Quaternion(0,0,0,0));
+    }
+    
 }
