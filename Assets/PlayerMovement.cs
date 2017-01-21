3081be17d4e17b6ac66d3c051d29046c90ad2588
azapalac <azapalac@ucsc.edu>
2017-01-20 20:27:10 -0800

    Movement first pass

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float movementLimit = 3;
+    public float moveSpeed;
+    public float currentMovementPosition = 0;
+
+    Vector2 isoLeft, isoRight;
+
+	// Use this for initialization
+	void Start () {
+        isoLeft = Vector2.up + Vector2.left;
+        isoLeft.Normalize();
+
+        isoRight = Vector2.down + Vector2.right;
+        isoRight.Normalize();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) 
+            && currentMovementPosition <= movementLimit)
+        {
+            transform.Translate(isoLeft*moveSpeed*Time.deltaTime);
+            currentMovementPosition += moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)
+            && currentMovementPosition >= -1*movementLimit)
+        {
+            transform.Translate(isoRight*moveSpeed*Time.deltaTime);
+            currentMovementPosition -= moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
+        {
+            //Move background faster
+        }
+    }
+}
