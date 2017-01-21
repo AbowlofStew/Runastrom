6fc922d7d07bf375beea782ec951389d4428fcc2
Seshelle <seshelle@ucsc.edu>
2017-01-21 11:57:18 -0800

    Almost_Perfect_Scrolling_Background

--- Assets/Background_Tile_Spawner.cs
+++ Assets/Background_Tile_Spawner.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class Background_Tile_Spawner : MonoBehaviour {
     public GameObject Tile;
-    private GameObject Tile2;
+    private float xSize;
+    private float ySize;
+    private Vector3 screenUpperRight;
     // Use this for initialization
     void Start () {
-        Vector3 p = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 1.35f));
-        Tile.transform.position = p;
-        float xSize = Tile.GetComponent<SpriteRenderer>().bounds.size.x;
+        screenUpperRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 1, 1));
+        Vector3 bottomRight = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, 1));
+        Vector3 screenSize = new Vector3(screenUpperRight.x - bottomRight.x, screenUpperRight.y - bottomRight.y, 0);
+        Vector3 p = screenUpperRight;
+        xSize = Tile.GetComponent<SpriteRenderer>().bounds.size.x;
+        ySize = Tile.GetComponent<SpriteRenderer>().bounds.size.y;
+        p.Set(p.x + xSize, p.y + ySize, p.z);
 
-        Tile2 = Instantiate(Tile, new Vector3(xSize, 0, 0), Quaternion.identity);
+        for (int j = 0; j < 7; j++)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                Instantiate(Tile, p, Quaternion.identity);
+                p.Set(p.x - screenSize.x / 14, p.y, p.z);
+            }
+            p.Set(screenUpperRight.x + xSize, p.y - screenSize.y / 6, p.z);
+        }
     }
 	
 	// Update is called once per frame
