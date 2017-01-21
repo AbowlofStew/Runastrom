ac2613e125cfe0b1e34a763c4799c0aafbcfc3ed
azapalac <azapalac@ucsc.edu>
2017-01-20 21:58:39 -0800

    Movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float movementLimit = 3;
     public float maxMoveSpeed, minMoveSpeed;
-    public float moveSpeed;
-    public float currentMovementPosition = 0;
+    private float moveSpeed;
+    private float currentMovementPosition = 0;
 
     Vector2 isoLeft, isoRight;
+    public enum KeysPressed
+    {
+        NoKeysPressed,
+        LeftKeyPressed,
+        RightKeyPressed,
+        BothKeysPressed
+    };
+    private KeysPressed LastKeyPressed;
 
 	// Use this for initialization
 	void Start () {
         isoLeft = Vector2.up + Vector2.left;
         isoLeft.Normalize();
-
+        moveSpeed = minMoveSpeed;
         isoRight = Vector2.down + Vector2.right;
         isoRight.Normalize();
 	}
@@ -25,20 +33,48 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow) 
             && currentMovementPosition <= movementLimit)
         {
+            if (LastKeyPressed == KeysPressed.RightKeyPressed)
+            {
+                moveSpeed = minMoveSpeed;
+            }
+
+            if (moveSpeed < maxMoveSpeed)
+            {
+                moveSpeed += Time.deltaTime;
+            }
             transform.Translate(isoLeft*moveSpeed*Time.deltaTime);
             currentMovementPosition += moveSpeed * Time.deltaTime;
+            LastKeyPressed = KeysPressed.LeftKeyPressed;
         }
 
         if (Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)
             && currentMovementPosition >= -1*movementLimit)
         {
+            if(LastKeyPressed == KeysPressed.LeftKeyPressed)
+            {
+                moveSpeed = minMoveSpeed;
+            }
+
+            if(moveSpeed< maxMoveSpeed)
+            {
+                moveSpeed += Time.deltaTime;
+            }
             transform.Translate(isoRight*moveSpeed*Time.deltaTime);
             currentMovementPosition -= moveSpeed * Time.deltaTime;
+            LastKeyPressed = KeysPressed.RightKeyPressed;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.RightArrow))
         {
+            moveSpeed = minMoveSpeed;
             //Move background faster
+            LastKeyPressed = KeysPressed.BothKeysPressed;
+        }
+
+        if(!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+        {
+            moveSpeed = minMoveSpeed;
+            LastKeyPressed = KeysPressed.NoKeysPressed;
         }
     }
 }
