69ede9763b3c14fa2f0ea8d5bf320514aec3d896
acarerk <acarerk@gmail.com>
2017-01-22 11:27:13 -0800

    obs

---
+++ Assets/Scripts/ObstacleBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleBehaviour : MonoBehaviour {
+    float deathTimer;
+    float lifespan = 20;
+    Vector2 direction;
+    float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+        setup();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        move();
+        deathTimer += Time.deltaTime;
+        if (deathTimer >= lifespan)
+        {
+            selfDestruct();
+        }
+    }
+    public void setup()
+    {
+        direction = Vector2.down + Vector2.left;
+        direction.Normalize();
+    }
+    public void move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+    public void changeSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+    public void selfDestruct()
+    {
+        GameObject.Find("Spawner Manager").GetComponent<ObstacleHandler>().obsList.Remove(this.gameObject);
+        Destroy(this);
+    }
+}
---
+++ Assets/Scripts/ObstacleHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleHandler : MonoBehaviour {
+    public float currentSpeed;
+    float timer;
+    public float spawnRate = 1;
+    public GameObject obs;
+    public List<GameObject> obsList = new List<GameObject>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+
+        if(timer >= spawnRate)
+        {
+            
+            spawnObs();
+            timer = 0;
+        }
+       
+	}
+    public void spawnObs()
+    {
+        float x = Random.Range(100, 300);
+        x = x / 100;
+        float y = (-1*x) + 4;
+       
+        Vector2 spawnloc = new Vector2(x, y);
+        GameObject obs1 = GameObject.Instantiate(obs, spawnloc, new Quaternion(0, 0, 0, 0));
+        obsList.Add(obs1);
+        obs1.GetComponent<ObstacleBehaviour>().changeSpeed(currentSpeed);
+    }
+    public void changeSpeed(float new_)
+    {
+        foreach (GameObject obs_ in obsList)
+        {
+            obs_.GetComponent<ObstacleBehaviour>().changeSpeed(new_);
+        }
+    }
+
+}
